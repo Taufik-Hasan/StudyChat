ee9bc5b7dd7172f56928edb1c2fc27e118645e15
Taufik Hasan <70083764+Taufik-Hasan@users.noreply.github.com>
2024-02-07 01:51:36 +0600

    Established N-Layers Architecture with Core Model Design
    Created a strong framework for the project using N-Layers architecture. Built different layers like Core, Data Access, Services, and Presentation. Also, organized folders and started working on the Core Model Design, setting up a good starting point for the project.

---
+++ StudyChat.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudyChat.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
